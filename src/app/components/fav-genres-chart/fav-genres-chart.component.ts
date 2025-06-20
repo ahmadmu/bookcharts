1370e849c9d20414d5754b6187aed9f1bf492153
Ahmad Moudallal <ahmad.moudallal.contractor@draeger.com>
2025-06-20 22:32:29 +0200

    hide sidebar initially, move genre import to modal

--- src/app/components/fav-genres-chart/fav-genres-chart.component.ts
+++ src/app/components/fav-genres-chart/fav-genres-chart.component.ts
@@ -1,8 +1,11 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { GenericRatingBarChart } from "../../shared/generic-rating-bar-chart/generic-rating-bar-chart.component";
 import { BooksService } from '../../services/books.service';
+import { faCheck, faCopy, faDownload, faFileImport } from '@fortawesome/free-solid-svg-icons';
+import { Book } from '../../models/api.model';
+import { ModalComponent } from '../../shared/modal/modal.component';
 
 @Component({
   selector: 'app-fav-genres-chart',
@@ -10,8 +13,10 @@ import { BooksService } from '../../services/books.service';
   imports: [
     FontAwesomeModule,
     FormsModule,
-    GenericRatingBarChart
-],
+    GenericRatingBarChart,
+    ModalComponent,
+    FontAwesomeModule
+  ],
   templateUrl: './fav-genres-chart.component.html',
   styleUrl: './fav-genres-chart.component.scss'
 })
@@ -26,4 +31,85 @@ export class FavGenresChartComponent {
     'all',
     ...this.booksService.getYears()
   ]
+
+  copyStatus: 'done' | string = 'Copy prompt to clipboard';
+  faCopy = faCopy;
+  faCheck = faCheck;
+  faDownload = faDownload;
+  faFileImport = faFileImport;
+
+  importGenresModal = signal(false)
+  csvMetadata = {name: '', size: 0, type: ''}
+
+  onGenresImport(event: any) {
+    const file = event.files.item(0);
+    this.readFile(file!!);
+  }    
+
+  readFile(file: File) {
+    this.csvMetadata = {
+      name: file.name,
+      size: file.size,
+      type: file.type
+    }
+
+    let reader: FileReader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = (e) => {
+      let csv: string = reader.result as string;
+      this.parseCsv(csv);
+    }
+  }
+
+  parseCsv(csvText: string) {
+    const commaOutsideOfQuoteRegex = /;(?=(?:(?:[^"]*"){2})*[^"]*$)/
+    const rows = csvText.split('\n')
+    rows.shift();
+    const contentRows = rows.map(row => row.split(commaOutsideOfQuoteRegex));
+    const books = this.booksService.books();
+    books.forEach((book: Book) => {
+      contentRows.forEach(contentRow => { 
+        if (book.title === contentRow[0]) {
+          book.genres = contentRow[1].split(',').map(genre => genre.trim());
+        }
+      })
+    })
+    this.booksService.books.set(books);
+    this.booksService.saveInLocalStorage();
+    this.importGenresModal.set(false);
+  }
+
+  exportTitleAndIds(event: Event) {
+    event.preventDefault();
+    event.stopPropagation();
+    
+    const titles = this.booksService.books().map(item => ({title: item.title}))
+    const header = ['Title', 'Genres'].join(';').concat('\n');
+    const csvContent = titles.map(item => item.title + ';').join('\n');
+
+    const BOM = "\uFEFF";
+    const data = BOM + header.concat(csvContent);
+    const blob = new Blob([data], { type: 'data:text/csv;charset=utf-8' })
+    var link = document.createElement('a');
+    link.setAttribute("href", URL.createObjectURL(blob));
+    link.setAttribute("download", "book_titles.csv");
+    document.body.appendChild(link);
+
+    link.click();
+  }
+
+  copyPromptToClipboard(event: Event) {
+    event.preventDefault();
+    event.stopPropagation();
+    const prompt = 'create a new semicolon separated csv based on the attached csv, and fill the genres field with the genres of each book';
+    navigator.clipboard.writeText(prompt).then(() => {
+      this.copyStatus = 'Copied!';
+    }).catch(err => {
+      this.copyStatus = 'Failed to copy prompt. Please try again.';
+      console.error('Failed to copy prompt: ', err);
+    });
+    setTimeout(() => {
+      this.copyStatus = 'Copy prompt to clipboard';
+    }, 5000);
+  }
 }
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,12 +1,11 @@
-import { Component, inject, signal } from '@angular/core';
-import { OpenApiDoc, OpenLibService } from '../services/open-lib.service';
+import { Component, computed, inject, signal } from '@angular/core';
+import { OpenLibService } from '../services/open-lib.service';
 import { BooksService } from '../services/books.service';
-import { catchError, finalize, merge, of, tap } from 'rxjs';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { ModalComponent } from "../shared/modal/modal.component";
 import { Book } from '../models/api.model';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faBook, faChartBar, faChartSimple, faCheck, faCopy, faDownload, faFileImport, faList, faShuffle, faTable, faTableList, faTrash, faUpload } from '@fortawesome/free-solid-svg-icons';
+import { faBook, faChartSimple, faCheck, faCopy, faDownload, faFileImport, faList, faShuffle, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-sidebar',
@@ -17,7 +16,7 @@ import { faBook, faChartBar, faChartSimple, faCheck, faCopy, faDownload, faFileI
     FontAwesomeModule,
   ],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
 
@@ -25,19 +24,14 @@ export class SidebarComponent {
   booksService = inject(BooksService)
 
   clearModal = signal(false)
-  importGenresModal = signal(false)
 
-  csvMetadata = {name: '', size: 0, type: ''}
+  hasData = computed(() => this.booksService.books().length > 0);
 
   faBook = faBook;
   faChartSimple = faChartSimple;
   faList = faList;
   faShuffle = faShuffle;
-  faDownload = faDownload;
-  faFileImport = faFileImport;
   faTrash = faTrash;
-  faCopy = faCopy;
-  faCheck = faCheck;
 
   copyStatus: 'done' | string = 'Copy prompt to clipboard';
 
@@ -51,72 +45,5 @@ export class SidebarComponent {
   clear() {
     localStorage.removeItem('data');
     this.booksService.books.set([]);
-  }
-
-  exportTitleAndIds() {
-    const titles = this.booksService.books().map(item => ({title: item.title}))
-    const header = ['Title', 'Genres'].join(';').concat('\n');
-    const csvContent = titles.map(item => item.title + ';').join('\n');
-
-    const BOM = "\uFEFF";
-    const data = BOM + header.concat(csvContent);
-    const blob = new Blob([data], { type: 'data:text/csv;charset=utf-8' })
-    var link = document.createElement('a');
-    link.setAttribute("href", URL.createObjectURL(blob));
-    link.setAttribute("download", "book_titles.csv");
-    document.body.appendChild(link);
-
-    link.click();
-  }
-
-  onGenresImport(event: any) {
-    const file = event.files.item(0);
-    this.readFile(file!!);
-  }    
-
-  readFile(file: File) {
-    this.csvMetadata = {
-      name: file.name,
-      size: file.size,
-      type: file.type
-    }
-
-    let reader: FileReader = new FileReader();
-    reader.readAsText(file);
-    reader.onload = (e) => {
-      let csv: string = reader.result as string;
-      this.parseCsv(csv);
-    }
-  }
-
-  parseCsv(csvText: string) {
-    const commaOutsideOfQuoteRegex = /;(?=(?:(?:[^"]*"){2})*[^"]*$)/
-    const rows = csvText.split('\n')
-    rows.shift();
-    const contentRows = rows.map(row => row.split(commaOutsideOfQuoteRegex));
-    const books = this.booksService.books();
-    books.forEach((book: Book) => {
-      contentRows.forEach(contentRow => { 
-        if (book.title === contentRow[0]) {
-          book.genres = contentRow[1].split(',').map(genre => genre.trim());
-        }
-      })
-    })
-    this.booksService.books.set(books);
-    this.booksService.saveInLocalStorage();
-    this.importGenresModal.set(false);
-  }
-
-  copyPromptToClipboard() {
-    const prompt = 'create a new semicolon separated csv based on the attached csv, and fill the genres field with the genres of each book';
-    navigator.clipboard.writeText(prompt).then(() => {
-      this.copyStatus = 'Copied!';
-    }).catch(err => {
-      this.copyStatus = 'Failed to copy prompt. Please try again.';
-      console.error('Failed to copy prompt: ', err);
-    });
-    setTimeout(() => {
-      this.copyStatus = 'Copy prompt to clipboard';
-    }, 5000);
   }
 }
